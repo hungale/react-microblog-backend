49a9dc1bd99def6e9b14c75444b6c0990936add4
diana <diliang4783573@gmail.com>
2020-08-24 15:44:12 -0700

    updated db.js for heroku database usage

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 /** Database connection for Microblog. */
-
+const dotenv = require('dotenv').config();
 const { Client } = require("pg");
 
-const client = new Client(process.env.DATABASE_URL || "postgresql:///microblog");
+const client = process.env.NODE_ENV === 'development' 
+                ? new Client(process.env.DATABASE_URL)
+                : new Client("postgresql:///microblog")
 
 client.connect();
-
 
 module.exports = client;
